4268d14a6bf9a07a9ba948cb26b85aec23e3d918
Clay Branch <clay@movementstrategy.com>
2014-05-11 13:36:36 -0400

    added crosshair to player controller

---
+++ Assets/Crosshair.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+
+	public Texture2D mainTexture;
+	Rect drawPosition;
+	// Use this for initialization
+	void Start () {
+		drawPosition = new Rect((Screen.width - mainTexture.width) / 2, (Screen.height - mainTexture.height) /2, mainTexture.width, mainTexture.height);
+		Debug.Log (drawPosition);
+	}
+
+	void OnGUI() {
+		GUI.DrawTexture(drawPosition, mainTexture);
+	}
+
+}
