15ee28bbfc2107746c246b4551d535c47cfa67f5
Clay Branch <clay@movementstrategy.com>
2014-06-15 14:23:15 -0600

    added an incremental clicking sound to the switch

---
+++ Assets/IncrementalClickSound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncrementalClickSound : MonoBehaviour {
+
+	public string soundName = "none";
+	private float lastPosition = 0f;
+	public float incrementAmount = .5f;
+	// Use this for initialization
+	void Start () {
+		MechanicalMove targetMechanicalMove = gameObject.GetComponent<MechanicalMove>();
+		targetMechanicalMove.OnMechanicalMove += onLockMove;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void onLockMove(Vector2 position, Vector2 speed) {
+		float amountMoved = Mathf.Abs (lastPosition - position.x);
+		if(amountMoved >= incrementAmount) {
+			lastPosition = position.x;
+			AudioController.Play (soundName);
+		}
+
+	}
+}
