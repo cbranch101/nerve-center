237adeac60fe7764c32fa2f9b711bf8e2e6f0eab
Clay Branch <clay@movementstrategy.com>
2014-07-27 12:10:30 -0600

    added boot up sounds

---
+++ Assets/PostPickerSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostPickerSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		turnOn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void turnOn() {
+		AudioController.Play ("Bootup");
+		AudioController.Play ("Ambient bootup");
+	}
+}
