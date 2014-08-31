7d8ffcdb8d9073a8945038edec309b89c9c983a6
ykantbobreed <ykantbobreed@hotmail.com>
2014-08-31 12:02:30 -0600

    Added the final switch, and set up its animCTRL.
    Made one or two changed to some MATs as well.

---
+++ Assets/Scripts/TEMP_ControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEMP_ControllerTest : MonoBehaviour {
+
+	public bool started;
+	[Range (0,1)]
+	public float pulledUp;
+	[Range (0,1)]
+	public float lock1;
+	[Range (0,1)]
+	public float lock2;
+	[Range (0,1)]
+	public float lock3;
+	[Range (0,1)]
+	public float leverPulled;
+
+	public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("Cyl_Started", started);
+		anim.SetFloat ("Cyl_Pulled", pulledUp);
+		anim.SetFloat ("Lock1_Pulled", lock1);
+		anim.SetFloat ("Lock2_Pulled", lock2);
+		anim.SetFloat ("Lock3_Pulled", lock3);
+		anim.SetFloat ("Lever_Pulled", leverPulled);
+	}
+}
