c5e3c1f2f96fc320b9ee0c3e7cef3041ca095c75
Clay Branch <clay@movementstrategy.com>
2014-09-27 16:07:04 -0600

    got the basics of the switch working

---
+++ Assets/FinalSwitch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalSwitch : MonoBehaviour {
+
+	private Animator switchAnimator;
+
+	// Use this for initialization
+	void Start () {
+		switchAnimator = GameObject.Find ("NASA_Control_FinalSwitch").GetComponent<Animator>();
+		switchAnimator.SetBool("Cyl_Started", true);
+		switchAnimator.SetFloat("Cyl_Pulled", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
