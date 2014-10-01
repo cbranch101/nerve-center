e428fc65d9275295034791a40fd51a479f55d566
Clay Branch <clay@movementstrategy.com>
2014-09-30 20:55:34 -0600

    scripting out power switch

---
+++ Assets/TextPanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextPanel : MonoBehaviour {
+
+	public PowerSwitch powerSwitch;
+	private Material screenMaterial;
+	// Use this for initialization
+	void Start () {
+
+		powerSwitch.OnPowerUp += onPowerUp;
+		powerSwitch.OnPowerDown += onPowerDown;
+		screenMaterial = gameObject.renderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void onPowerUp() {
+		screenMaterial.SetFloat("_ScanLinesIntensity", 0.666f);
+	}
+
+	void onPowerDown() {
+		screenMaterial.SetFloat("_ScanLinesIntensity", 0f);
+	}
+}
