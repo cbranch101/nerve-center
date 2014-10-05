6d1cd58a12678f817b7c7fcc7b81d292c7a8e454
ykantbobreed <ykantbobreed@hotmail.com>
2014-10-05 11:42:03 -0600

    Material instancing tests....

---
+++ Assets/TestMaterialInstances.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMaterialInstances : MonoBehaviour {
+
+	public bool powerSwitch;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (powerSwitch == true)
+		{
+			this.renderer.material.SetFloat ("_ScreenImageBrightness", 1.5f);
+			this.renderer.material.SetFloat ("_ScanGradientIntensity", 1);
+		}
+		if (powerSwitch == false)
+		{
+			this.renderer.material.SetFloat ("_ScreenImageBrightness", 0);
+			this.renderer.material.SetFloat ("_ScanGradientIntensity", 0);
+		}
+	}
+}
