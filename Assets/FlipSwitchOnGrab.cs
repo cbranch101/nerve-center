82cffb28d0667e641b4e34ed7b9c1a411a34c9e7
Clay Branch <clay@movementstrategy.com>
2014-10-08 20:23:54 -0600

    toggle switches are working and switch game is basically working again

---
+++ Assets/FlipSwitchOnGrab.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipSwitchOnGrab : MonoBehaviour {
+
+	public GameObject objectWithAnimator;
+	private Animator animator;
+	public delegate void FlipAction();
+	public event FlipAction OnFlipUp;
+	public event FlipAction OnFlipDown;
+	public GrabTarget grabTarget;
+	public string clickSoundName;
+	private bool flipped = true;
+	
+	// Use this for initialization
+	void Start () {
+		grabTarget.OnGrabEnter += onGrabEnter;
+		grabTarget.OnGrabExit += onGrabExit;
+		animator = objectWithAnimator.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void onGrabEnter() {
+		flipped = flipped ? false : true;
+		animator.SetBool ("Power", flipped);
+		if(clickSoundName != null) {
+			AudioController.Play (clickSoundName);
+		}
+
+		if(flipped && (OnFlipDown != null)) {
+			OnFlipDown();
+		}
+
+		if(!flipped && (OnFlipUp != null)) {
+			OnFlipUp();
+		}
+
+	}	
+
+	public void onGrabExit() {
+
+	}
+
+}
